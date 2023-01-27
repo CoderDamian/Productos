98eb1d886d5f8061742422fb6872fca89eca608a
Damian <AdrianAndradeChristian@outlook.com>
2023-01-27 14:31:59 -0500

    Implementing persistence layer

---
+++ MyProduct/MyProduct.Domain/Seedwork/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace MyProduct.Domain.Seedwork
+{
+    public interface IRepository<T> where T : class
+    {
+        Task AddAsync(T entity);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task DeleteAsync(int ID);
+        void UpdateAsync(T entity);
+    }
+}
---
+++ MyProduct/MyProduct.Persistence/Seedwork/RepositoryBase.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyProduct.Domain.Seedwork;
+
+namespace MyProduct.Persistence.Seedwork
+{
+    public abstract class RepositoryBase<T> : IRepository<T> where T : class
+    {
+        private readonly MyDbContext _myDbContext;
+
+        public RepositoryBase(MyDbContext myDbContext)
+        {
+            _myDbContext = myDbContext;
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _myDbContext.Set<T>().AddAsync(entity);
+        }
+
+        public async Task DeleteAsync(int ID)
+        {
+            T? entity = await _myDbContext.Set<T>().FindAsync(ID);
+
+            if (entity == null)
+                throw new NullReferenceException(nameof(entity));
+
+            _myDbContext.Remove(entity);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _myDbContext.Set<T>().ToListAsync();
+        }
+
+        public void UpdateAsync(T entity)
+        {
+            _myDbContext.Set<T>().Update(entity);
+        }
+    }
+}
